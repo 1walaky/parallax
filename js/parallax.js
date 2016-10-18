c1eef466bcbc9438a4c868feba332d91accce919
1walaky <bodo.batya@gmail.com>
2016-10-18 16:28:44 +0200

    parallax kesz

---
+++ js/parallax.js
@@ -0,0 +1,55 @@
+$(function(){
+
+  
+  var $window         = $(window);
+  var $anim_elements  = $('.parallax');
+
+
+  function check_if_view(){
+    var window_top    = $window.scrollTop();
+    var window_height = $window.outerHeight();
+    var window_bottom = window_top + window_height;
+    $.each($anim_elements, function(){
+
+      //animáció hozzáadása a megjelenítendő elemhez
+      var $anim_el = $($(this).find('h1')[0]);
+      var anim_el_top = $anim_el.position().top;
+      var anim_el_height = $anim_el.outerHeight();
+      var anim_el_bottom = anim_el_top + anim_el_height;
+
+      //Itt vagyunk, amikor az adott parallax elem látható lesz
+      if(window_bottom >= anim_el_top && anim_el_bottom >= window_top){
+
+        //Hozzáadjuk az osztályt az animálandó elemhez, ami 0-ra állítja a bal margint
+        //css transition miatt lesz animált
+        $anim_el.addClass('zeroLeftMargin');
+
+        //--Parallax varázslat--
+        //Ahhoz, hogy fotó is lehessen a parallaxos háttér, kell a parallax elem magassága.
+        //A parallax háttérkép eredeti magassága. 
+        //(a háttérnek az eredeti kép mérete szerint kell megjelennie a weblapon)
+        //A parallax elem magassága - a kép magassága = a kép y kezdő koordinátája
+        //Kell még egy szorzószám, ami a képernyő aktuális helyzetéhez igazítja
+        //a háttérképet scrollozás közben (amikor a parallax elem megjelenik, 
+        //a háttérnek az elem alján kell lennie, amikor eltűnik, akkor az elem tetején)
+        var screenX = $window.scrollTop();
+        var $para_el = $(this);
+        var para_el_top = $para_el.position().top;
+        var para_el_height = $para_el.outerHeight();
+        var para_el_bottom = para_el_top + para_el_height;
+        var bgYPos = Math.abs((screenX - para_el_bottom) / (window_height + para_el_height));
+
+        var bg_image_url = $para_el.css('background-image');
+        var bg_image_regex = /"([^"]*)"/;
+        var img_url = bg_image_regex.exec(bg_image_url)[1];
+        var img = new Image;
+        img.src = img_url
+        var img_height = img.height;
+        $para_el.css('background-position', '50% '+(para_el_height - img_height)*bgYPos+'px');
+      }else{
+        $anim_el.removeClass('zeroLeftMargin');
+      }
+    });
+  }
+  $window.on('scroll', check_if_view);
+});
---
+++ parallax.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Parallax</title>
+  <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+<section class="module parallax parallax-1">
+  <div class="container">
+    <h1>Serene</h1>
+  </div>
+</section>
+
+<section class="module content">
+  <div class="container">
+    <h2>Lorem Ipsum Dolor</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+  </div>
+</section>
+
+<section class="module parallax parallax-2">
+  <div class="container">
+    <h1>Rise</h1>
+  </div>
+</section>
+
+<section class="module content">
+  <div class="container">
+    <h2>Lorem Ipsum Dolor</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+  </div>
+</section>
+
+<section class="module parallax parallax-3">
+  <div class="container">
+    <h1>Calm</h1>
+  </div>
+</section>
+
+<section class="module content">
+  <div class="container">
+    <h2>Lorem Ipsum Dolor</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+  </div>
+</section>
+</div>
+<script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+<script src="./js/parallax.js"></script>
+</body>
+</html>
